488f3f8067e6dfd0ce401a8b121a4284faccccfd
TallNutAlarak <1194291667@qq.com>
2020-04-21 15:49:34 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     outputDir: 'render',
+//     assetsDir: 'static',
+//     devServer:{
+//         open:true,
+//         host:'127.0.0.1',
+//         port:8000
+//     }
+// }
